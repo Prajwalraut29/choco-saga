3e365ebcc0f416bc35c169c8c16ff7bf4dee7a82
prajwal raut <90103440+Prajwalraut29@users.noreply.github.com>
2024-03-31 23:31:18 +0530

    build : chocklate-saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './store/hooks';
+import { createBoard } from './utils/createBoard';
+import { moveBelow, updateBoard } from './store';
+import Board from './components/Board';
+import { isColumnOfFour } from './utils/moveCheckLogic';
+import { formulaForColumnOfFour } from './utils/formulas';
 
 function App() {
+  const dispatch = useAppDispatch()
+  const board = useAppSelector(({ candyCrush: { board } }) => board)
+  const boardSize = useAppSelector(({ candyCrush: { boardSize } }) => boardSize)
+
+
+  useEffect(() => {
+    dispatch(updateBoard(createBoard(boardSize)))
+
+  }, [boardSize, dispatch])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const newBoard = [...board];
+      isColumnOfFour(newBoard, boardSize, formulaForColumnOfFour(boardSize))
+      dispatch(updateBoard(newBoard))
+      dispatch(moveBelow())
+    }, 150);
+    return () => clearInterval(timeout)
+  }, [board, boardSize, dispatch])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <p className='text-3xl font-bold font-serif bg-orange-300 rounded-full my-3'>Chocklate Saga</p>
+      <Board />
     </div>
   );
 }
---
+++ src/store/hooks.ts
@@ -0,0 +1,5 @@
+import { useSelector, useDispatch, TypedUseSelectorHook } from "react-redux";
+import { AppDispatch, RootState } from ".";
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
